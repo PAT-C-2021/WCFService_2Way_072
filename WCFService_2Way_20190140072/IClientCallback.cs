eeba097684c4e3e58b787970bb88e4efd950b1b3
anisanurfadila <anisanfd925@gmail.com>
2022-01-07 23:19:34 +0700

    Add project files.

---
+++ WCFService_2Way_20190140072/IClientCallback.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WCFService_2Way_20190140072
+{
+        [ServiceContract]
+        interface IClientCallback
+        {
+            [OperationContract(IsOneWay = true)]
+            void pesanKirim(string user, string pesan);
+        }
+
+}
